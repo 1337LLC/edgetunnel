bb0ee37f47993333e5b6ca7cf55b6b866c74268b
1337LLC <admin@1337llc.com>
2026-06-14 09:27:51 +0000

    modified:   src/worker-with-socks5-experimental.js

--- src/worker-with-socks5-experimental.js
+++ src/worker-with-socks5-experimental.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd342d11e-d484-4583-b36e-528ab1f0afa4';
 
 let proxyIP = '';
 
@@ -18,7 +18,7 @@ if (!isValidUUID(userID)) {
 }
 
 let parsedSocks5Address = {}; 
-let enableSocks = false;
+let enableSocks = true;
 
 export default {
 	/**
